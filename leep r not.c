548e3dfaf2ad9c2f3f83dd362c416b9195ae6400
sanjeev65 <35800660+sanjeev65@users.noreply.github.com>
2018-02-01 16:51:34 +0530

    Create leep r not.c

---
+++ leep r not.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+ int a;
+ printf("enter the year a=");
+ scanf("%d",&a);
+ if(a%4==0)
+ printf("the year is leep");
+ else
+ printf("the year is not leep");
+}
