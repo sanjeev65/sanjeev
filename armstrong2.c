6c7f19c1adaecf9eabe878f99ba66bf7ecef8a66
sanjeev65 <35800660+sanjeev65@users.noreply.github.com>
2018-02-27 16:39:20 +0530

    Create armstrong2.c

---
+++ armstrong2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+	void main()
+	{
+	    int a,b,c=0,d=0;
+	    
+	    printf("enter the three number:");
+	    scanf("%d",&a);
+	    b=a;
+	    while(b!=0)
+	    {
+	        c=a%10;
+	        b=b+c;
+	        a=a%10;
+	    }
+	    if(d==a)
+	        printf("%d is an Armstrong number.",a);
+	    else
+	        printf("%d is not an Armstrong number.",a);
+
+	    
+	}
